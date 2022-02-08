3b11251d6613a6a76d308cb216cb101a04401831
Ella Svete <ella.svete@gmail.com>
2022-02-08 15:23:31 -0800

    Added 5 question prompts

--- js/app.js
+++ js/app.js
@@ -1,11 +1,55 @@
 'use strict';
 
-console.log('hey world!');
+ console.log('What\'s up!');
+
+let responseOne = prompt('What is your name?');
+
+  alert(`Welcome ${responseOne}!!!`);
+  console.log ('User entered in ' + responseOne);
 
 let answerOne = prompt('Do I have a cat?').toLowerCase();
 
 if(answerOne === 'yes' || answerOne === 'y'){
   alert('Yes I do have a cat');
+  console.log ('User entered in ' + answerOne);
 } else if(answerOne === 'no' || answerOne === 'n'){
   alert('sorry you are incorrect');
 }
+
+let answerTwo = prompt('Do I play music?').toUpperCase();
+
+if(answerTwo === 'YES' || answerTwo === 'Y'){
+  alert('You bet I do!');
+  console.log ('User entered in ' + answerTwo);
+} else if(answerTwo === 'NO' || answerTwo === 'N'){
+  alert('sorry you are incorrect');
+}
+
+let answerThree = prompt('Have I ever lived outside of USA?').toLowerCase();
+
+if(answerThree === 'yes' || answerThree === 'y'){
+  alert(`Correct ${responseOne}!! I lived in Chile for 10 months.`);
+  console.log ('User entered in ' + answerThree);
+} else if (answerThree === 'no' || answerThree === 'n'){
+  alert('Nice try!');
+}
+
+let answerFour = prompt('Do I like to cook?').toLowerCase();
+
+if(answerFour === 'yes' || answerFour === 'y'){
+  alert('Yes. I love cooking with my partner.');
+  console.log ('User entered in ' + answerFour);
+} else if(answerFour === 'no' || answerFour === 'n'){
+  alert('fail!');
+}
+
+let answerFive = prompt('Do I know how to juggle?').toLowerCase();
+
+if(answerFive === 'yes' || answerFive ==='y'){
+  alert('Yep! I\'m a part-time clown.');
+  console.log ('User entered in ' + answerFive);
+} else if(answerFive === 'no' || answerFive === 'n'){
+  alert('Surprise!');
+}
+
+// let responseTwo = prompt (`Thanks for playing, ${responseOne} !!');
