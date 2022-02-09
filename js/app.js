917c5969e2e1c20944cd6220992877d085746827
Ella Svete <ella.svete@gmail.com>
2022-02-08 17:07:00 -0800

    commited out console.logs

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 'use strict';
 
- console.log('What\'s up!');
+//  console.log('What\'s up!');
 
 let responseOne = prompt('What is your name?');
 
   alert(`Welcome ${responseOne}! Ready to play a guessing game about me?`);
-  console.log ('User entered in ' + responseOne);
+  // console.log ('User entered in ' + responseOne);
 
 let answerOne = prompt('Do I know how to juggle?').toLowerCase();
 
 if(answerOne === 'yes' || answerOne === 'y'){
   alert('Yep. I\'m a part-time clown!');
-  console.log ('User entered in ' + answerOne);
+  // console.log ('User entered in ' + answerOne);
 } else if(answerOne === 'no' || answerOne === 'n'){
   alert('Wrong answer!');
 }
@@ -20,7 +20,7 @@ let answerTwo = prompt('Do I play music?').toUpperCase();
 
 if(answerTwo === 'YES' || answerTwo === 'Y'){
   alert('You bet I do!');
-  console.log ('User entered in ' + answerTwo);
+  // console.log ('User entered in ' + answerTwo);
 } else if(answerTwo === 'NO' || answerTwo === 'N'){
   alert('Sorry. You are incorrect.');
 }
@@ -29,7 +29,7 @@ let answerThree = prompt('Have I ever lived outside of USA?').toLowerCase();
 
 if(answerThree === 'yes' || answerThree === 'y'){
   alert(`Correct ${responseOne}!! I lived in Chile for 10 months.`);
-  console.log ('User entered in ' + answerThree);
+  // console.log ('User entered in ' + answerThree);
 } else if (answerThree === 'no' || answerThree === 'n'){
   alert('Nice try!');
 }
@@ -38,7 +38,7 @@ let answerFour = prompt('Do I like to cook?').toLowerCase();
 
 if(answerFour === 'yes' || answerFour === 'y'){
   alert('Yes. I love cooking with my partner.');
-  console.log ('User entered in ' + answerFour);
+  // console.log ('User entered in ' + answerFour);
 } else if(answerFour === 'no' || answerFour === 'n'){
   alert('fail!');
 }
@@ -47,7 +47,7 @@ let answerFive = prompt('Is my astrological sign Scorpio?').toLowerCase();
 
 if(answerFive === 'yes' || answerFive ==='y'){
   alert('No way! I\'m an Aquarius.');
-  console.log ('User entered in ' + answerFive);
+  // console.log ('User entered in ' + answerFive);
 } else if(answerFive === 'no' || answerFive === 'n'){
   alert('That\'s right! I\'m an Aquarius!');
 }
