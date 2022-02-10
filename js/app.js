a5725085d4548c8e01112a1d4828b5574204dc51
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-02-10 13:52:38 -0800

    added question 1 function

--- js/app.js
+++ js/app.js
@@ -2,105 +2,110 @@
 
 console.log('What\'s up!');
 let counter = 0;
-let responseOne = prompt('What is your name?'); 
+let responseOne = prompt('What is your name?');
 
-  alert(`Welcome ${responseOne}! Ready to play a guessing game about me?`);
-  console.log ('User entered in ' + responseOne);
+alert(`Welcome ${responseOne}! Ready to play a guessing game about me?`);
+console.log('User entered in ' + responseOne);
 
-let answerOne = prompt('Do I know how to juggle?').toLowerCase(); 
+function questionOne() {
+let answerOne = prompt('Do I know how to juggle?').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y'){
-  counter++;
-  console.log(counter);
-  alert('Yep. I\'m a part-time clown!');
-  console.log ('User entered in ' + answerOne);
-} else if(answerOne === 'no' || answerOne === 'n'){
-  alert('Wrong answer!');
+  if (answerOne === 'yes' || answerOne === 'y') {
+    counter++;
+    console.log(counter);
+    alert('Yep. I\'m a part-time clown!');
+    console.log('User entered in ' + answerOne);
+  } else if (answerOne === 'no' || answerOne === 'n') {
+    alert('Wrong answer!');
+  }
+
 }
+questionOne();
 
-let answerTwo = prompt('Do I play music?').toUpperCase(); 
+let answerTwo = prompt('Do I play music?').toUpperCase();
 
-if(answerTwo === 'YES' || answerTwo === 'Y'){
+if (answerTwo === 'YES' || answerTwo === 'Y') {
   counter++;
   alert('You bet I do!');
-  console.log ('User entered in ' + answerTwo);
-} else if(answerTwo === 'NO' || answerTwo === 'N'){
+  console.log('User entered in ' + answerTwo);
+} else if (answerTwo === 'NO' || answerTwo === 'N') {
   alert('Sorry. You are incorrect.');
 }
 
 let answerThree = prompt('Have I ever lived outside of USA?').toLowerCase();
 
-if(answerThree === 'yes' || answerThree === 'y'){
+if (answerThree === 'yes' || answerThree === 'y') {
   counter++;
   alert(`Correct ${responseOne}!! I lived in Chile for 10 months.`);
-  console.log ('User entered in ' + answerThree);
-} else if (answerThree === 'no' || answerThree === 'n'){
+  console.log('User entered in ' + answerThree);
+} else if (answerThree === 'no' || answerThree === 'n') {
   alert('Nice try!');
 }
 
 let answerFour = prompt('Do I like to cook?').toLowerCase();
 
-if(answerFour === 'yes' || answerFour === 'y'){
+if (answerFour === 'yes' || answerFour === 'y') {
   counter++;
   alert('Yes. I love cooking with my partner.');
-  console.log ('User entered in ' + answerFour);
-} else if(answerFour === 'no' || answerFour === 'n'){
+  console.log('User entered in ' + answerFour);
+} else if (answerFour === 'no' || answerFour === 'n') {
   alert('fail!');
 }
 
 let answerFive = prompt('Is my astrological sign Scorpio?').toLowerCase();
 
-if(answerFive === 'yes' || answerFive ==='y'){
+if (answerFive === 'yes' || answerFive === 'y') {
   counter++;
   alert('No way! I\'m an Aquarius.');
-  console.log ('User entered in ' + answerFive);
-} else if(answerFive === 'no' || answerFive === 'n'){
+  console.log('User entered in ' + answerFive);
+} else if (answerFive === 'no' || answerFive === 'n') {
   alert('That\'s right! I\'m an Aquarius!');
 }
 
 
 let correctNumber = 1;
 let userGuesses = 4;
-  
-while(userGuesses !== 0){
+
+while (userGuesses !== 0) {
   userGuesses--;
 
   let userNumber = prompt('How many cats do I have?').toLowerCase();
-  if(+userNumber === correctNumber){
+  if (+userNumber === correctNumber) {
     counter++;
     alert('you are correct!');
-    userGuesses = 0; }
-  if(userNumber > 1 ) {
+    userGuesses = 0;
+  }
+  if (userNumber > 1) {
     alert('too high');
   }
-  if(userNumber < 1 ) {
+  if (userNumber < 1) {
     alert('too low');
   }
-  if(userGuesses === 0){
+  if (userGuesses === 0) {
     alert('I have 1 cat.');
   }
 }
 
-let userGuessesTwo = 6;  
-let places = ['Greece' , 'Vietnam' , 'France']
+let userGuessesTwo = 6;
+let places = ['Greece', 'Vietnam', 'France']
 //  console.log(places.length);
 
-while(userGuessesTwo < 0) {
+while (userGuessesTwo < 0) {
   userGuessesTwo--;
-   let userAnswer = prompt('Where are my top three places to travel?').toLowerCase();
-   
-for(let i = 0; i < places.length; i++) {
-  console.log(userAnswer)
-   if(userAnswer === places[i]) {
-    counter++;
-     alert('Nicely done!');
-     userGuessesTwo = 1;
+  let userAnswer = prompt('Where are my top three places to travel?').toLowerCase();
+
+  for (let i = 0; i < places.length; i++) {
+    console.log(userAnswer)
+    if (userAnswer === places[i]) {
+      counter++;
+      alert('Nicely done!');
+      userGuessesTwo = 1;
+    }
   }
 }
-}
-while(userGuessesTwo === 6){
-   alert('My top three places to travel are Greece, Vietnam, and France.')
-   break;
+while (userGuessesTwo === 6) {
+  alert('My top three places to travel are Greece, Vietnam, and France.')
+  break;
 }
 //    let userAnswer;
 //        userAnswer = ''
@@ -109,4 +114,4 @@ while(userGuessesTwo === 6){
 //
 
 
-  alert(`Thanks for playing ${responseOne}! You got ${counter} correct out of 7!`);
+alert(`Thanks for playing ${responseOne}! You got ${counter} correct out of 7!`);
