134d8ef72aac58e4c3433ee72f9893d551eda92e
Ella Svete <ella.svete@gmail.com>
2022-02-09 18:20:20 -0800

    nested loop

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 'use strict';
 
- console.log('What\'s up!');
-let counter = 0
-// let responseOne = prompt('What is your name?'); 
+console.log('What\'s up!');
+let counter = 0;
+let responseOne = prompt('What is your name?'); 
 
   alert(`Welcome ${responseOne}! Ready to play a guessing game about me?`);
   console.log ('User entered in ' + responseOne);
 
-// let answerOne = prompt('Do I know how to juggle?').toLowerCase(); 
+let answerOne = prompt('Do I know how to juggle?').toLowerCase(); 
 
 if(answerOne === 'yes' || answerOne === 'y'){
-  counter++
+  counter++;
   console.log(counter);
   alert('Yep. I\'m a part-time clown!');
   console.log ('User entered in ' + answerOne);
@@ -18,40 +18,40 @@ if(answerOne === 'yes' || answerOne === 'y'){
   alert('Wrong answer!');
 }
 
-// let answerTwo = prompt('Do I play music?').toUpperCase(); 
+let answerTwo = prompt('Do I play music?').toUpperCase(); 
 
 if(answerTwo === 'YES' || answerTwo === 'Y'){
-  counter++
+  counter++;
   alert('You bet I do!');
   console.log ('User entered in ' + answerTwo);
 } else if(answerTwo === 'NO' || answerTwo === 'N'){
   alert('Sorry. You are incorrect.');
 }
 
-// let answerThree = prompt('Have I ever lived outside of USA?').toLowerCase();
+let answerThree = prompt('Have I ever lived outside of USA?').toLowerCase();
 
 if(answerThree === 'yes' || answerThree === 'y'){
-  counter++
+  counter++;
   alert(`Correct ${responseOne}!! I lived in Chile for 10 months.`);
   console.log ('User entered in ' + answerThree);
 } else if (answerThree === 'no' || answerThree === 'n'){
   alert('Nice try!');
 }
 
-// let answerFour = prompt('Do I like to cook?').toLowerCase();
+let answerFour = prompt('Do I like to cook?').toLowerCase();
 
 if(answerFour === 'yes' || answerFour === 'y'){
-  counter++
+  counter++;
   alert('Yes. I love cooking with my partner.');
   console.log ('User entered in ' + answerFour);
 } else if(answerFour === 'no' || answerFour === 'n'){
   alert('fail!');
 }
 
-// let answerFive = prompt('Is my astrological sign Scorpio?').toLowerCase();
+let answerFive = prompt('Is my astrological sign Scorpio?').toLowerCase();
 
 if(answerFive === 'yes' || answerFive ==='y'){
-  counter++
+  counter++;
   alert('No way! I\'m an Aquarius.');
   console.log ('User entered in ' + answerFive);
 } else if(answerFive === 'no' || answerFive === 'n'){
@@ -59,44 +59,45 @@ if(answerFive === 'yes' || answerFive ==='y'){
 }
 
 
-let correctNumber = 1;
-var userGuesses = 4;
+let correctNumber = '1';
+let userGuesses = 4;
   
-  while(userGuesses !== 0){
-    userGuesses--
-  let userNumber = +prompt('How many cats do I have?');
-    if(userNumber === correctNumber){
-      counter++
-      alert('you are correct!');
-    // break; }
-    if(userNumber > 1 ) {
-      alert('too high');
-    }
-    if(userNumber < 1 ) {
-      alert('too low');
-    }
-    if(userGuesses === 0){
-      alert('Nice try. I have 1 cat');
-    }
+while(userGuesses !== 0){
+  userGuesses--;
+  let userNumber = prompt('How many cats do I have?');
+  if(userNumber === correctNumber){
+    counter++;
+    alert('you are correct!');
+    userGuesses = 0; }
+  if(userNumber > 1 ) {
+    alert('too high');
   }
+  if(userNumber < 1 ) {
+    alert('too low');
+  }
+  if(userGuesses === 0){
+    alert('I have 1 cat.');
+  }
+}
 
-//  let places = ['Greece' , 'Vietnam' , 'France']
-// //  console.log(places.length);
-// var userGuessesTwo = 6;
+  let places = ['Greece' , 'Vietnam' , 'France']
+//  console.log(places.length);
+  let userGuessesTwo = 6;
 
-// for(let i = 0; i < userGuessesTwo; i++) {
-//   let userAnswer = prompt('Where are my top three places to travel?');
+for(let i = 0; i < userGuessesTwo; i++) {
+   let userAnswer = prompt('Where are my top three places to travel?');
 
-//   for(let j = 0; j < places.length; j++) {
-//     if(userAnswer === places[j]) {
-//     // counter++
-//     alert('Nicely done!');
-//     break;
-//   }
-// }
-  // if(userAnswer === )
-    // alert('Close, but not close enough!');
-// }
+for(let j = 0; j < places.length; j++) {
+   if(userAnswer === places[j]) {
+    counter++;
+   alert('Nicely done!');
+   break;
+  }
+}
+  if(userAnswer === ) 
+    alert('Close, but not close enough!');
+}
 
 
-  // alert(`Thanks for playing ${responseOne}! You got ${counter} correct out of 7!`);
+  { alert(`Thanks for playing ${responseOne}! You got ${counter} correct out of 7!`);
+}
